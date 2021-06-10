28d4ba3f77b9e69bc0fb532211eb3ead1da4b3e2
Anibal Santos <anibalsantosgo@gmail.com>
2021-06-10 19:11:03 +0200

    clean template

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+import '@/css/tailwind.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { ThemeProvider } from 'next-themes'
+import { DefaultSeo } from 'next-seo'
+import Head from 'next/head'
+
+import { SEO } from '@/components/SEO'
+import LayoutWrapper from '@/components/LayoutWrapper'
+
+export default function App({ Component, pageProps }) {
+  return (
+    <ThemeProvider attribute="class">
+      <Head>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+      </Head>
+      <DefaultSeo {...SEO} />
+      <LayoutWrapper>
+        <Component {...pageProps} />
+      </LayoutWrapper>
+    </ThemeProvider>
+  )
 }
-
-export default MyApp
